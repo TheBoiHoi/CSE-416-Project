8c54a68c9c23165f1d21c8ce4aee8dff13117ddd
YL <yuqiang.lin@stonybrook.edu>
2022-03-27 21:37:54 -0400

    added completeTrade function

--- server/resolvers/user-resolver.js
+++ server/resolvers/user-resolver.js
@@ -44,7 +44,6 @@ const createPendingTrade = async(req, res)=>{
     const {sellerId, buyerId, itemId} = req.body
     const seller=await User.findOne({_id:sellerId})
     const buyer=await User.findOne({_id:buyerId})
-    const item = await Item.findOne({_id:itemId})
     const newTrade= new PendingTrade({
         buyer_id:buyerId,
         seller_id:sellerId,
@@ -52,11 +51,50 @@ const createPendingTrade = async(req, res)=>{
         seller_status:false,
         item_id:itemId
     })
+    seller.pending_trades.push(newTrade._id)
+    buyer.pending_trades.push(newTrade._id)
+    await User.updateOne({_id:sellerId}, {pending_trades:seller.pending_trades})
+    await User.updateOne({_id:buyerId}, {pending_trades:buyer.pending_trades})
     const saved=await newTrade.save()
     if(saved){
-        return res.status(200).json({msg:"OK"})
+        return res.status(200).json({msg:"OK", tradeId:newTrade._id})
     }
     return res.status(404).json({msg:"ERROR"})
+}
+
+const completeTrade = async(req, res)=>{
+    const {tradeId}=req.body
+    const trade=await PendingTrade.findOne({_id:tradeId})
+    const {buyer_id, seller_id, item_id}=trade
+
+    const seller=await User.findOne({_id:seller_id})
+    const buyer=await User.findOne({_id:buyer_id})
+
+    await Item.updateOne({_id:item_id}, {owner:buyer_id})
+
+    //remove item from the seller
+    const sellerItems=seller.items_owned
+    sellerItems.filter(item => item!=item_id)
+    
+
+    //remove pending trade from seller, add complete trade to seller
+    const sellerPending=seller.pending_trades
+    sellerPending.filter(trade => trade!=tradeId)
+    const sellerComplete=seller.completed_trades
+    sellerComplete.push(tradeId)
+    await User.updateOne({_id:seller_id}, {items_owned:sellerItems, pending_trades:sellerPending, completed_trades:sellerComplete})
+
+    //push the item to the buyer
+    const buyerItems=buyer.items_owned
+    buyerItems.push(item_id)
+
+    //remove pending trade from buyer, add pending trade to buyer
+    const buyerPending=buyer.pending_trades
+    buyerPending.filter(trade => trade!=tradeId)
+    const buyerComplete=buyer.completed_trades
+    buyerComplete.push(tradeId)
+    await User.updateOne({_id:buyer_id}, {items_owned:buyerItems, pending_trades:buyerPending, completed_trades:buyerComplete})
+    return res.status(200).json({msg:"OK"})
 }
 
 module.exports = {
