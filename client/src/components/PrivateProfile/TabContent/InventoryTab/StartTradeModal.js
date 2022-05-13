ba8d77713ace722af4c6dabbcd121560f269d3d8
yupenglu <ylu1100@bths.edu>
2022-05-13 15:25:15 -0400

    merge

--- client/src/components/PrivateProfile/TabContent/InventoryTab/StartTradeModal.js
+++ client/src/components/PrivateProfile/TabContent/InventoryTab/StartTradeModal.js
@@ -2,12 +2,19 @@ import {useState,useEffect} from 'react'
 import {Modal,Button} from 'react-bootstrap'
 import {useNavigate} from 'react-router-dom'
 import axios from 'axios'
+import { TramRounded } from '@mui/icons-material'
 const StartTradeModal=(props)=> {
     //make request to get transaction info 
     //props.curr_user= current user id
     //props.user=user id of the profile
     //props.item.itemId= item id
-    const requestTrade=()=>{
+    const requestTrade=async ()=>{
+        let trade=await axios.post('/user/trade/create',{
+            sellerId:props.user,
+            buyerId:props.curr_user,
+            itemId:props.item.itemId
+        })
+        console.log(trade)
         props.setShow(false)
     }
     const handleClose=()=>{
