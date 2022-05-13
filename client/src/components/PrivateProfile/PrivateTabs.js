e80ce2fc6f0f13c1f300fc23e34c6d7f7ab742ff
Jie Zhang <jiezhang@Jies-MacBook-Air.local>
2022-05-13 18:15:58 -0400

    added loading spinner for pending trades

--- client/src/components/PrivateProfile/PrivateTabs.js
+++ client/src/components/PrivateProfile/PrivateTabs.js
@@ -1,29 +1,25 @@
 import React, {useState, useEffect} from 'react';
-import {Tab,Tabs} from 'react-bootstrap';
+import {Tab,Tabs, Spinner} from 'react-bootstrap';
 import ExchangesTab from './TabContent/ExchangesTab/ExchangesTab';
 import InventoryTab  from './TabContent/InventoryTab/InventoryTab';
 import PendingTab from './TabContent/PendingTab/PendingTab';
 import PendingModal from './TabContent/PendingTab/PendingModal';
 import axios from 'axios'
+import './TabContent/PendingTab/pendingTab.css';
 
 const PrivateTabs=(props)=>{
     //PENDING MODAL
     const [show, setShow] = useState(false);
     const [trade, setTrade] = useState({});
-    const [item, setItem] = useState({});
-    const [buyer, setBuyer] = useState({});
-    const [seller, setSeller] = useState({});
     const [disabled, setDisabled] = useState(false);
+    const [loading, setLoading] = useState (false);
+    const [buttonShow, setButtonShow] = useState(true);
 
-    const showModal = (trade, item, buyer, seller) => {
+    const showModal = (trade) => {
         setShow(true);
         setTrade(trade);
-        setItem(item);
-        setBuyer(buyer);
-        setSeller(seller);
         if((trade.seller_id == props.user.userId && trade.seller_status == true)
                 || (trade.buyer_id == props.user.userId && trade.buyer_status == true)){
-            console.log("setting true")
             setDisabled(true);
         }else{
             setDisabled(false);
@@ -32,9 +28,14 @@ const PrivateTabs=(props)=>{
     
     const hideModal = () => {
         setShow(false);
+        setLoading(false);
+        setButtonShow(true);
     }
 
     const confirmTrade = async () => {
+        setDisabled(true);
+        setLoading(true);
+        setButtonShow(false);
         const data = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/user/trade/update-status`, {
             tradeId: trade._id,
             userId: props.user.userId
@@ -42,17 +43,14 @@ const PrivateTabs=(props)=>{
         if(data){
             const trade = data.data;
             if(trade.buyer_status == true && trade.seller_status == true){
-                console.log("completing trades");
                 const res = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/user/trade/complete`, {
                     tradeId: trade._id
                 });
                 if(res.data.message == 'Error'){
                     window.alert("Transaction not completed! Error encountered!");
-                    // console.log("TRANSACTION NOT COMPLETED")
                 }
             }
         }
-        setDisabled(true);
         await fetchPendingTrades();
         hideModal();
     }
@@ -112,18 +110,21 @@ const PrivateTabs=(props)=>{
 
 
     return(
-    <Tabs style={{width:"50%"}} defaultActiveKey="Exchanges" id="uncontrolled-tab-example" className="mb-3">
-        <Tab style={{width:"50%",boxShadow: "1px 1px 1px #9E9E9E"}} eventKey="Exchanges" title="Exchanges">
-            <ExchangesTab  user={props.user}></ExchangesTab>
-        </Tab>
-        <Tab style={{width:"50%",boxShadow: "1px 1px 1px #9E9E9E"}} eventKey="Inventory" title="Inventory">
-            <InventoryTab user={props.user}></InventoryTab>
-        </Tab>
-        <Tab style={{width:"50%",boxShadow: "1px 1px 1px #9E9E9E"}} eventKey="Pending" title="Pending" >
-            <PendingTab pendings={trades} handleShowModal={showModal}></PendingTab>
-            <PendingModal handleConfirm={confirmTrade} show={show} trade={trade} buyer={buyer} seller={seller} item={item} hide={hideModal} disabled={disabled}/>
-        </Tab>
-    </Tabs>
+        <>
+            <Tabs style={{width:"50%"}} defaultActiveKey="Exchanges" id="uncontrolled-tab-example" className="mb-3">
+                <Tab style={{width:"50%",boxShadow: "1px 1px 1px #9E9E9E"}} eventKey="Exchanges" title="Exchanges">
+                    <ExchangesTab  user={props.user}></ExchangesTab>
+                </Tab>
+                <Tab style={{width:"50%",boxShadow: "1px 1px 1px #9E9E9E"}} eventKey="Inventory" title="Inventory">
+                    <InventoryTab user={props.user}></InventoryTab>
+                </Tab>
+                <Tab style={{width:"50%",boxShadow: "1px 1px 1px #9E9E9E"}} eventKey="Pending" title="Pending" >
+                    <PendingTab pendings={trades} handleShowModal={showModal}></PendingTab>
+                    <PendingModal buttonShow={buttonShow} handleConfirm={confirmTrade} show={show} hide={hideModal} disabled={disabled}/>
+                </Tab>
+            </Tabs>
+            {loading && <Spinner className="loading" animation="grow" />}
+        </>
     )
 };
 export default PrivateTabs
